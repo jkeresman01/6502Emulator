bb5a674c2a688f3508ecc989fba782031dcd0a83
Josip <josipkeresman@gmail.com>
2025-04-19 23:06:32 +0200

    Add test for LDA Immediate

--- tests/cpu/LDATests.cpp
+++ tests/cpu/LDATests.cpp
@@ -23,13 +23,110 @@ class CPU6502Test : public ::testing::Test
     }
 };
 
-TEST_F(CPU6502Test, LDAImmediate)
+TEST_F(CPU6502Test, WillLDALoadPositiveValueCorrectlyImmediate)
 {
+    // GIVEN
+    Byte carryBefore = cpu.GetStatusFlags().C;
+    Byte interruptBefore = cpu.GetStatusFlags().I;
+    Byte decimalBefore = cpu.GetStatusFlags().D;
+    Byte overflowBefore = cpu.GetStatusFlags().C;
+    Byte notUsedFlagBefore = cpu.GetStatusFlags()._;
+
+    // WHEN
     Memory::Write(0x8000, 0xA9);
     Memory::Write(0x8001, 0x42);
 
     cpu.Reset();
     cpu.Step();
 
+    // THEN
     EXPECT_EQ(cpu.GetAccumulator(), 0x42);
+
+    EXPECT_FALSE(cpu.GetStatusFlags().N);
+    EXPECT_FALSE(cpu.GetStatusFlags().Z);
+
+    // Status flags not changed
+    Byte carryAfter = cpu.GetStatusFlags().C;
+    Byte interruptAfter = cpu.GetStatusFlags().I;
+    Byte decimalAfter = cpu.GetStatusFlags().D;
+    Byte overflowAfter = cpu.GetStatusFlags().C;
+    Byte notUsedFlagAfter = cpu.GetStatusFlags()._;
+
+    EXPECT_EQ(carryBefore, carryAfter);
+    EXPECT_EQ(interruptBefore, interruptAfter);
+    EXPECT_EQ(decimalBefore, decimalAfter);
+    EXPECT_EQ(overflowBefore, overflowAfter);
+    EXPECT_EQ(notUsedFlagBefore, notUsedFlagAfter);
+}
+
+TEST_F(CPU6502Test, WillLDALoadZeroValueCorrectlyImmediate)
+{
+    // GIVEN
+    Byte carryBefore = cpu.GetStatusFlags().C;
+    Byte interruptBefore = cpu.GetStatusFlags().I;
+    Byte decimalBefore = cpu.GetStatusFlags().D;
+    Byte overflowBefore = cpu.GetStatusFlags().C;
+    Byte notUsedFlagBefore = cpu.GetStatusFlags()._;
+
+    // WHEN
+    Memory::Write(0x8000, 0xA9);
+    Memory::Write(0x8001, 0x00);
+
+    cpu.Reset();
+    cpu.Step();
+
+    // THEN
+    EXPECT_EQ(cpu.GetAccumulator(), 0x00);
+
+    EXPECT_EQ(cpu.GetStatusFlags().N, 0);
+    EXPECT_EQ(cpu.GetStatusFlags().Z, 1);
+
+    // Status flags not changed
+    Byte carryAfter = cpu.GetStatusFlags().C;
+    Byte interruptAfter = cpu.GetStatusFlags().I;
+    Byte decimalAfter = cpu.GetStatusFlags().D;
+    Byte overflowAfter = cpu.GetStatusFlags().C;
+    Byte notUsedFlagAfter = cpu.GetStatusFlags()._;
+
+    EXPECT_EQ(carryBefore, carryAfter);
+    EXPECT_EQ(interruptBefore, interruptAfter);
+    EXPECT_EQ(decimalBefore, decimalAfter);
+    EXPECT_EQ(overflowBefore, overflowAfter);
+    EXPECT_EQ(notUsedFlagBefore, notUsedFlagAfter);
+}
+
+TEST_F(CPU6502Test, WillLDALoadNegativeValueCorrectlyImmediate)
+{
+    // GIVEN
+    Byte carryBefore = cpu.GetStatusFlags().C;
+    Byte interruptBefore = cpu.GetStatusFlags().I;
+    Byte decimalBefore = cpu.GetStatusFlags().D;
+    Byte overflowBefore = cpu.GetStatusFlags().V;
+    Byte notUsedFlagBefore = cpu.GetStatusFlags()._;
+
+    // WHEN
+    Memory::Write(0x8000, 0xA9);
+    Memory::Write(0x8001, 0xFF);
+
+    cpu.Reset();
+    cpu.Step();
+
+    // THEN
+    EXPECT_EQ(cpu.GetAccumulator(), 0xFF);
+
+    EXPECT_EQ(cpu.GetStatusFlags().N, 1);
+    EXPECT_EQ(cpu.GetStatusFlags().Z, 0);
+
+    // Unrelated flags should remain unchanged
+    Byte carryAfter = cpu.GetStatusFlags().C;
+    Byte interruptAfter = cpu.GetStatusFlags().I;
+    Byte decimalAfter = cpu.GetStatusFlags().D;
+    Byte overflowAfter = cpu.GetStatusFlags().V;
+    Byte notUsedFlagAfter = cpu.GetStatusFlags()._;
+
+    EXPECT_EQ(carryBefore, carryAfter);
+    EXPECT_EQ(interruptBefore, interruptAfter);
+    EXPECT_EQ(decimalBefore, decimalAfter);
+    EXPECT_EQ(overflowBefore, overflowAfter);
+    EXPECT_EQ(notUsedFlagBefore, notUsedFlagAfter);
 }
