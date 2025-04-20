c4b61e89900797a36b26d0d70bd5f169ec004f41
Josip <josipkeresman@gmail.com>
2025-04-20 18:26:06 +0200

    Add unit tests for SEC, SED && SEI

---
+++ tests/cpu/SECTests.cpp
@@ -0,0 +1,47 @@
+#include "../src/Core/CPU6502.h"
+#include "../src/Core/Memory.h"
+
+#include <gtest/gtest.h>
+
+using namespace emulator6502;
+
+class CPU6502SECTests : public ::testing::Test
+{
+  protected:
+    CPU6502 cpu;
+
+    void SetUp() override
+    {
+        Memory::Reset();
+        cpu.Init();
+    }
+
+    void TearDown() override
+    {
+        cpu.Reset();
+        Memory::Reset();
+    }
+};
+
+TEST_F(CPU6502SECTests, WillSetCarryFlagCorrectly)
+{
+    Byte interruptBefore = cpu.GetStatusFlags().I;
+    Byte decimalBefore = cpu.GetStatusFlags().D;
+    Byte overflowBefore = cpu.GetStatusFlags().V;
+    Byte notUsedBefore = cpu.GetStatusFlags()._;
+    Byte zeroBefore = cpu.GetStatusFlags().Z;
+    Byte negativeBefore = cpu.GetStatusFlags().N;
+
+    Memory::Write(0x8000, 0x38); // SEC
+
+    cpu.Step();
+
+    EXPECT_EQ(cpu.GetStatusFlags().C, 1);
+
+    EXPECT_EQ(cpu.GetStatusFlags().I, interruptBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().D, decimalBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().V, overflowBefore);
+    EXPECT_EQ(cpu.GetStatusFlags()._, notUsedBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().Z, zeroBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().N, negativeBefore);
+}
---
+++ tests/cpu/SEDTests.cpp
@@ -0,0 +1,47 @@
+#include "../src/Core/CPU6502.h"
+#include "../src/Core/Memory.h"
+
+#include <gtest/gtest.h>
+
+using namespace emulator6502;
+
+class CPU6502SEDTests : public ::testing::Test
+{
+  protected:
+    CPU6502 cpu;
+
+    void SetUp() override
+    {
+        Memory::Reset();
+        cpu.Init();
+    }
+
+    void TearDown() override
+    {
+        cpu.Reset();
+        Memory::Reset();
+    }
+};
+
+TEST_F(CPU6502SEDTests, WillSetDecimalFlagCorrectly)
+{
+    Byte carryBefore = cpu.GetStatusFlags().C;
+    Byte interruptBefore = cpu.GetStatusFlags().I;
+    Byte overflowBefore = cpu.GetStatusFlags().V;
+    Byte notUsedBefore = cpu.GetStatusFlags()._;
+    Byte zeroBefore = cpu.GetStatusFlags().Z;
+    Byte negativeBefore = cpu.GetStatusFlags().N;
+
+    Memory::Write(0x8000, 0xF8); // SED
+
+    cpu.Step();
+
+    EXPECT_EQ(cpu.GetStatusFlags().D, 1);
+
+    EXPECT_EQ(cpu.GetStatusFlags().C, carryBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().I, interruptBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().V, overflowBefore);
+    EXPECT_EQ(cpu.GetStatusFlags()._, notUsedBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().Z, zeroBefore);
+    EXPECT_EQ(cpu.GetStatusFlags().N, negativeBefore);
+}
